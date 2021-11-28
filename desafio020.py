0086f16e64ddcfdac3f8d347f6dd1d0e16a25ab9
Ricardo Silva <ricktk.rs@gmail.com>
2021-11-28 13:09:38 -0300

    Correção do desafio 020.py

--- desafio020.py
+++ desafio020.py
@@ -1,8 +1,8 @@
 from random import shuffle
-n1 = str(input('Nome do primeiro nome: '))
-n2 = str(input('Nome do segundo nome: '))
-n3 = str(input('Nome do terceiro nome: '))
-n4 = str(input('Nome do quarto número: '))
+n1 = str(input('Nome do primeiro aluno: '))
+n2 = str(input('Nome do segundo aluno: '))
+n3 = str(input('Nome do terceiro aluno: '))
+n4 = str(input('Nome do quarto aluno: '))
 lista = [n1,n2,n3,n4]
 shuffle(lista)
 print('A orderm de apresentação dos trabalhos será {}.'.format(lista))
